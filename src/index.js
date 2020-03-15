24d82ba75631bb41ec8d03ff8cd4f33239040789
Unalo <Unalomtshokotsha@gmail.com>
2020-03-16 00:38:39 +0200

    fixed comment error

--- src/index.js
+++ src/index.js
@@ -111,7 +111,7 @@ import './index.css';
     <Game />,
     document.getElementById('root')
   );
-  // finding winner 
+  // getting winner 
   function calculateWinner(squares) {
     const lines = [
       [0, 1, 2],
@@ -122,7 +122,7 @@ import './index.css';
       [2, 5, 8],
       [0, 4, 8],
       [2, 4, 6],
-    ];
+    ]
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
